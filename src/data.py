7c548f1f6ce328e17238d4a515a873badf07c51e
akashrajkn <akashrajkn@gmail.com>
2019-03-26 18:20:04 +0100

    Process task 1 and task 3 data

---
+++ src/data.py
@@ -0,0 +1,103 @@
+import os
+import pickle
+
+
+def read_file(path, task):
+    '''
+    Read file and return respective dictionary
+    Args:
+        path: file path
+        task: task number
+
+    Returns:
+        out : dictionary
+    '''
+    if path is None:
+        print('File path not specified')
+        return
+
+    if   task == 1:
+        out = read_task_1(path)
+    elif task == 2:
+        out = read_task_2(path)
+    elif task == 3:
+        out = read_task_3(path)
+    else
+        print('Task number is wrong')
+
+    return out
+
+
+def process_MSD(msd):
+    '''
+    Process morphosyntactic descriptions (MSDs) in the input sentence
+    Args:
+        msd: string containing different MSDs
+    Returns:
+        out: dict with different msds
+    '''
+
+    out = {}
+    msds = msd.strip().split(',')
+
+    for m in msds:
+        current = m.strip().split('=')
+        out[current[0]] = current[1]
+
+    return out
+
+
+def read_task_1(path):
+    '''
+    Task 1 data: lemma  MSD  target_form
+    Args:
+        path: file path
+    Returns:
+        out : dictionary
+    '''
+    with open(path, 'r', encoding='utf-8') as f:
+        source = f.read()
+
+    out       = []
+    sentences = source.strip().split('\n')
+
+    for sentence in sentences:
+        line = sentence.strip().split('\t')
+
+        if len(line) > 3:
+            print('Something wrong with line: {}'.format(sentence))
+            continue
+
+        out.append({
+            'lemma'      : line[0],
+            'MSD'        : process_MSD(line[1]),
+            'target_form': line[2]
+        })
+
+
+def read_task_3(path):
+    '''
+    Task 3 data: source_form  MSD  target_form
+    Args:
+        path: file path
+    Returns:
+        out : dictionary
+    '''
+    with open(path, 'r', encoding='utf-8') as f:
+        source = f.read()
+
+    out       = []
+    sentences = source.strip().split('\n')
+
+    for sentence in sentences:
+        line = sentence.strip().split('\t')
+
+        if len(line) > 3:
+            print('Something wrong with line: {}'.format(sentence))
+            continue
+
+        out.append({
+            'source_form': line[0],
+            'MSD'        : process_MSD(line[1]),
+            'target_form': line[2]
+        })
