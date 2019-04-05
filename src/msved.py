b43fd7ca171c93bd52e7266813628fe0f32e7e9b
akashrajkn <akashrajkn@gmail.com>
2019-04-05 17:19:46 +0200

    MSVED - Encoder added

---
+++ src/msved.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+
+class MSVED(nn.Module):
+    def __init__(self, h_dim, z_dim, vocab_size, msd_size, bidirectional=True):
+        super(MSVED, self).__init__()
+
+        self.h_dim         = h_dim  # u_dim = 2 * h_dim for bidirectional
+        self.z_dim         = z_dim
+        self.vocab_size    = vocab_size
+        self.msd_size      = msd_size
+        self.bidirectional = bidirectional
+
+        # Encoder   --   TODO: Check if I can replace it with GRU layer
+        self.rnn_forward  = nn.GRUCell(input_size=vocab_size, hidden_size=h_dim)
+        self.rnn_backward = nn.GRUCell(input_size=vocab_size, hidden_size=h_dim)
+
+        self.z_mu     = nn.Linear(2 * h_dim, z_dim)
+        self.z_logvar = nn.Linear(2 * h_dim, z_dim)
+
+    def encode(x_s):
+        source_len = x_s.size()[0]
+        h_forward  = torch.zeros(self.hidden_size)
+        h_backward = torch.zeros(self.hidden_size)
+
+        for i in range(source_len):
+            h_forward  = self.rnn_forward(x_s[i], h_forward)
+            h_backward = self.rnn_backward(x_s[source_len - i - 1], h_backward)
+
+        # u is the hidden representation of x_s
+        u      = torch.cat((h_forward, h_backward), 0)
+        mu     = F.relu(self.z_mu(u))
+        logvar = F.relu(self.z_logvar(u))
+
+        # Reparameterize: z = mu + sigma * epsilon
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+
+        return mu + eps * std
+
+    def decoder():
+        pass
+
+    def forward(self, x_s, y_t):
+
+        z   = self.encode(x_s)
+        x_t = self.decode(z, y_t)
+
+        return x_t
