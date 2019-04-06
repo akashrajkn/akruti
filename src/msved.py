a78cc7e739950b08035780c929003d479f884847
akashrajkn <akashrajkn@gmail.com>
2019-04-06 16:37:52 +0200

    Comments

--- src/msved.py
+++ src/msved.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 torch.manual_seed(1)
 
 
@@ -10,7 +11,8 @@ class MSVED(nn.Module):
     def __init__(self, h_dim, z_dim, vocab_size, msd_size, bidirectional=True):
         super(MSVED, self).__init__()
 
-        self.h_dim         = h_dim  # u_dim = 2 * h_dim for bidirectional
+        # u_dim = 2 * h_dim for bidirectional
+        self.h_dim         = h_dim
         self.z_dim         = z_dim
         self.vocab_size    = vocab_size
         self.msd_size      = msd_size
@@ -20,8 +22,11 @@ class MSVED(nn.Module):
         self.rnn_forward  = nn.GRUCell(input_size=vocab_size, hidden_size=h_dim)
         self.rnn_backward = nn.GRUCell(input_size=vocab_size, hidden_size=h_dim)
 
+        # TODO: initialize weight
         self.z_mu     = nn.Linear(2 * h_dim, z_dim)
         self.z_logvar = nn.Linear(2 * h_dim, z_dim)
+
+        # Decoder   -- TODO
 
     def encode(x_s):
         source_len = x_s.size()[0]
@@ -32,23 +37,26 @@ class MSVED(nn.Module):
             h_forward  = self.rnn_forward(x_s[i], h_forward)
             h_backward = self.rnn_backward(x_s[source_len - i - 1], h_backward)
 
-        # u is the hidden representation of x_s
+        # u      : hidden representation of x_s
         u      = torch.cat((h_forward, h_backward), 0)
         mu     = F.relu(self.z_mu(u))
         logvar = F.relu(self.z_logvar(u))
 
-        # Reparameterize: z = mu + sigma * epsilon
-        std = torch.exp(0.5*logvar)
+        # Reparam: z = mu + sigma * epsilon
+        std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
 
         return mu + eps * std
 
-    def decoder():
+    def decoder(z, y_t):
         pass
 
     def forward(self, x_s, y_t):
 
+        # z  : latent representation of lemma
         z   = self.encode(x_s)
+
+        # x_t: Re-inflected form
         x_t = self.decode(z, y_t)
 
         return x_t
