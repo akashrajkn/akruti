ddd31bf23a6db471038609b6401b86e172a0c4d4
akashraj <akashrajkn@gmail.com>
2019-05-07 01:15:03 +0200

    Added run files

--- src/run_script.sh
+++ src/run_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for VARIABLE in navajo arabic finnish german maltese 
 do
-    python preprocess.py -language=$VARIABLE
+    python preprocess.py -language=$VARIABLE --rewrite
+
     srun --gres=gpu:1 -t 2:00:00 python main.py --train -language=$VARIABLE -model_id=3 -epochs=180
     srun --gres=gpu:1            python main.py --test  -language=$VARIABLE -model_id=3
 done
