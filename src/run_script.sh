2deab18069bc5e8ffb5977174f43abaf13668b68
akashrajkn <akashrajkn@gmail.com>
2019-05-19 14:45:37 +0200

    Script changed

--- src/run_script.sh
+++ src/run_script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-MODELID = 1
+MODELID=11
 
 for VARIABLE in arabic finnish georgian german hungarian maltese navajo russian spanish turkish
 do
-    python preprocess.py -language=$VARIABLE --rewrite
-
     srun --gres=gpu:1 -t 15:00:00 python main.py  --train -language=$VARIABLE -model_id=$MODELID -epochs=200 -batch_size=32
-    srun --gres=gpu:1             python main.py  --test  -language=$VARIABLE -model_id=$MODELID
+    srun --gres=gpu:1 -t 15:00:00 python main.py  --test  -language=$VARIABLE -model_id=$MODELID
                                   python evalm.py         -language=$VARIABLE -model_id=$MODELID
 done
