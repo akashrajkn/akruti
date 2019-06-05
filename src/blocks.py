caf384642e940114b38370c4084dbe92b92d105f
akashrajkn <akashrajkn@gmail.com>
2019-06-05 21:43:49 +0200

    sum, mean for log prob and kl

--- src/blocks.py
+++ src/blocks.py
@@ -17,20 +17,24 @@ class VAE(nn.Module):
     '''
     Preliminaries: VAE
     '''
-    def __init__(self, input_dim, emb_dim, z_dim, h_dim, device, padding_idx=None):
+    def __init__(self, vocab_size, emb_dim, z_dim, h_dim, device, max_len, padding_idx=None):
         super(VAE, self).__init__()
 
-        self.input_dim = input_dim
-        self.z_dim     = z_dim
+        self.input_dim   = vocab_size
+        self.output_dim  = vocab_size
+        self.z_dim       = z_dim
         self.emb_dim     = emb_dim
-        self.h_dim     = h_dim
-        self.device    = device
+        self.h_dim       = h_dim
+        self.device      = device
+        self.vocab_size  = vocab_size
+        self.max_len     = max_len
         self.padding_idx = padding_idx
 
-        self.embedding = nn.Embedding(input_dim, e_dim, padding_idx=padding_idx)
-        self.rnn       = nn.GRU(emb_dim, h_dim, bidirectional=True)
-        self.fc_mu      = nn.Linear(h_dim * 2, z_dim)
-        self.fc_sigma   = nn.Linear(h_dim * 2, z_dim)
+        self.embedding = nn.Embedding(self.input_dim, e_dim, padding_idx=padding_idx)
+        self.rnn_enc   = nn.GRU(emb_dim, h_dim, bidirectional=True)
+        self.rnn_dec   = nn.GRU(z_dim, self.output_dim)
+        self.fc_mu     = nn.Linear(h_dim * 2, z_dim)
+        self.fc_sigma  = nn.Linear(h_dim * 2, z_dim)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -40,7 +44,7 @@ class VAE(nn.Module):
 
     def encoder(self, x):
         embedded  = self.embedding(x)
-        _, hidden = self.rnn(embedded)
+        _, hidden = self.rnn_enc(embedded)
         hidden    = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)
 
         mu        = F.relu(self.fc_mu(hidden))
@@ -48,10 +52,30 @@ class VAE(nn.Module):
 
         return hidden, mu, sigma
 
-    def decoder(self):
-        pass
+    def decoder(self, o, h, z):
+
+        input    = o.type(torch.LongTensor).to(self.device)
+        input    = input.unsqueeze(0)
+        embedded = self.embedding(input)
+
+
+
+
+
+
 
     def forward(self, x):
 
+        batch_size       = x.shape[1]
+        outputs          = torch.zeros(self.max_len, batch_size, self.vocab_size).to(self.device)
+
         h, z_mu, z_sigma = self.encoder(x)
         z                = self.reparameterize(z_mu, z_sigma)
+        o                = x[0, :]
+
+        for step in range(1, self.max_len):
+            o, h          = self.decoder(o, h, z)
+            outputs[step] = o
+            o             = o.max(1)[1]
+
+        return outputs, z_mu, z_sigma
